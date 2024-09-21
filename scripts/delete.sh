e3ee8697e1078615d0d19a29b525045e4fe71ab3
Tamim Shahriar <tash3080@gmail.com>
2024-09-22 01:32:29 +1000

    delete action test

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -2,4 +2,12 @@
 # Destroy the Node.js microservice on Kubernetes.
 #
 
+az aks get-credentials --resource-group $AZURE_RG --name $AZURE_AKS --overwrite-existing
+
+kubectl get pods
+kubectl get services
+
 envsubst < ./scripts/kubernetes/deployment.yaml | kubectl delete -f -
+
+kubectl get pods
+kubectl get services
