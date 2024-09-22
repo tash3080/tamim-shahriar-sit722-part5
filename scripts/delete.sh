2eb7e0fd760ec356655fbfe04300f1bdfc864e52
Tamim Shahriar <tash3080@gmail.com>
2024-09-22 11:47:44 +1000

    Final Commit all is working

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -5,9 +5,7 @@
 az aks get-credentials --resource-group $AZURE_RG --name $AZURE_AKS --overwrite-existing
 
 kubectl get pods
-kubectl get services
 
 envsubst < ./scripts/kubernetes/deployment.yaml | kubectl delete -f -
 
 kubectl get pods
-kubectl get services
